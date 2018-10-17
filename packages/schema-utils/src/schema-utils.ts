7b2a66c924e920de3ddd6d708b8162a9a2f2c23e
Evan Lovely <evanlovely@gmail.com>
2018-10-17 03:23:18 -0700

    feat(schema-utils): converting to Typescript
    affects: @basalt/base-schema-utils

--- packages/schema-utils/src/schema-utils.ts
+++ packages/schema-utils/src/schema-utils.ts
@@ -1,4 +1,4 @@
-const Ajv = require('ajv'); // https://github.com/epoberezkin/ajv
+import * as Ajv from 'ajv'; // https://github.com/epoberezkin/ajv
 
 const ajv = new Ajv();
 
@@ -6,15 +6,22 @@ const ajvDefaults = new Ajv({
   useDefaults: true,
 });
 
+interface InterfaceSchema {
+  $schema: string;
+  type: 'object';
+  properties: object;
+}
+
 /**
  * Validate Schema itself (metaSchema)
  * Validates against contents of the `$schema` field
  * @link https://github.com/epoberezkin/ajv#validateschemaobject-schema---boolean
- * @param {Object} schema - JSON Schema
- * @return {{ok: boolean, message: string}} - True if valid
  */
-function validateSchema(schema) {
-  const isSchemaValid = ajv.validateSchema(schema);
+export function validateSchema(schema: InterfaceSchema): {
+  ok: boolean,
+  message: string,
+} {
+  const isSchemaValid: boolean = ajv.validateSchema(schema);
   return {
     ok: isSchemaValid,
     message: isSchemaValid
@@ -31,7 +38,11 @@ function validateSchema(schema) {
  * @param {Object} data - Data to validate
  * @return {{ok: boolean, message: string, data: Object}} - Results
  */
-function validateSchemaAndAssignDefaults(schema, data) {
+export function validateSchemaAndAssignDefaults(schema: InterfaceSchema, data: object): {
+  ok: boolean,
+  message: string,
+  data: object, // @todo look into using "generics" to keep type info from original `data` param
+} {
   const { ok, message } = validateSchema(schema);
   if (!ok) {
     return {
@@ -43,7 +54,7 @@ function validateSchemaAndAssignDefaults(schema, data) {
 
   const newData = Object.assign({}, data);
   // This `validate` function mutates `newData`, so that's why we created a new object above first.
-  const isValid = ajvDefaults.validate(schema, newData);
+  const isValid: boolean = !!ajvDefaults.validate(schema, newData);
   return {
     ok: isValid,
     message: isValid ? '' : ajvDefaults.errorsText(),
@@ -54,11 +65,11 @@ function validateSchemaAndAssignDefaults(schema, data) {
 /**
  * Validate Data against Schema
  * @link https://github.com/epoberezkin/ajv
- * @param {Object} schema - JSON Schema
- * @param {Object} data - Data to validate
- * @return {{ok: boolean, message: string}} - Results
  */
-function validateDataAgainstSchema(schema, data) {
+export function validateDataAgainstSchema(schema: InterfaceSchema, data: object): {
+  ok: boolean,
+  message: string,
+} {
   const { ok, message } = validateSchema(schema);
   if (!ok) {
     return {
@@ -67,15 +78,9 @@ function validateDataAgainstSchema(schema, data) {
     };
   }
 
-  const isValid = ajv.validate(schema, data);
+  const isValid = !!ajv.validate(schema, data);
   return {
     ok: isValid,
     message: isValid ? '' : ajv.errorsText(),
   };
 }
-
-module.exports = {
-  validateSchema,
-  validateSchemaAndAssignDefaults,
-  validateDataAgainstSchema,
-};
